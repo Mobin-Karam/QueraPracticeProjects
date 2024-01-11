1b4932e65ac17f7d7d1c08888f0812dddd65de32
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-11 11:43:52 +0330

    add some changes

---
+++ week02/script.js
@@ -0,0 +1,37 @@
+//^ 1.syntax
+
+//^ 2. variable
+/**
+ *
+ * JsDoc
+ */
+
+//^ 2.1 scope
+// global scope
+// function scope
+// block scope
+// module scope
+
+//^ 2.2 hoisting
+// attach to blobal scope
+
+//^ 2.2.1
+// decleration 1.
+// initialazation 2.
+// use 3.
+
+//^ 2.3 variable comparison
+
+// console.log(title);
+// var title = "quera"; // data type: String
+// 1.scope
+// block scope -
+// function scope +
+
+// console.log(title)
+// {
+//   var title = "quera2";
+// }
+
+
+// 2. hoisting +
