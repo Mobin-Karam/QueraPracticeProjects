c152ce4c6938f4e8976501542853becde27d3665
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-12 00:38:04 +0330

    add some changes

--- week02/script.js
+++ week02/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //^ 1.syntax
 
 //^ 2. variable
@@ -22,16 +24,63 @@
 
 //^ 2.3 variable comparison
 
+// **** var :
 // console.log(title);
 // var title = "quera"; // data type: String
 // 1.scope
 // block scope -
 // function scope +
+// hoisting +
 
 // console.log(title)
 // {
 //   var title = "quera2";
 // }
 
+// **** let :
 
-// 2. hoisting +
+let fName; //declaration
+fName = "mohsen"; // initailazation
+
+// block scope +
+// hoist -
+
+// & let variable in function scope & block scope it's not be hoisting -
+
+// var name2 = "mobin";
+// var name2 = "mobin";
+// var name2 = "mobin";
+// var name2 = "mobin";
+// {
+//   var name2 = "mobin";
+// }
+
+// let name3 = "maha";
+// name3 = "maha";
+// name3 = "maha";
+{
+  //   console.log(name3);
+  //   let name3 = "maha";
+  //   console.log(name3);
+}
+
+// let name2;
+
+// {
+//   let name2 = () => {};
+// }
+
+// {
+//   let name2 = "ali";
+// }
+
+// {
+//   let name2 = 0;
+// }
+
+// const family = '     babaali    23    3';
+// family.charAt(2)
+// console.log(family.trim())
+
+const fullName = "pouriya      babaali";
+console.log(fullName.replaceAll(" ", ""));
