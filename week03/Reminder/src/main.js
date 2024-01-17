8c136575ca5c0b386ae7b0503c868876010369c7
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-17 21:47:10 +0330

    solve 99% of Reminder Question

--- week03/Reminder/src/main.js
+++ week03/Reminder/src/main.js
@@ -1,10 +1,63 @@
 // use this tasks to test your code
-import tasks from "./data.json";
+import tasks from "./data.json" assert { type: "json" };
+
+const currentDate = "2024-01-15T18:00:00Z";
 
 function reminder(data, currentDate) {
+  let nowDate = new Date(currentDate);
+  let [year, month, date, hours] = [
+    nowDate.getUTCFullYear(),
+    nowDate.getUTCMonth(),
+    nowDate.getUTCDate(),
+    nowDate.getUTCHours(),
+  ];
 
+  let result = [];
+  for (let x = 0; x < data.length; x++) {
+    let newDate = new Date(Date.parse(data[x].date));
+    let [year2, month2, date2, hours2] = [
+      newDate.getUTCFullYear(),
+      newDate.getUTCMonth(),
+      newDate.getUTCDate(),
+      newDate.getUTCHours(),
+    ];
+
+    // Set Difference Between Same Days
+    let diffHours = hours2 - hours;
+
+    // Make condition for push data to result
+    // if year equal to year2 go ahead
+    if (year === year2) {
+      if (month === month2) {
+        if (date === date2) {
+          // if Difference of Hours
+          if (diffHours >= 3) {
+            result.push(`Wait for it: ${data[x].title}`);
+          } else if (diffHours < 3 && diffHours >= 0) {
+            result.push(`Just do it: ${data[x].title}`);
+          } else if (diffHours < 0) {
+            result.push(`To late: ${data[x].title}`);
+          }
+        } else if (date > date2) {
+          result.push(`To late: ${data[x].title}`);
+        } else if (date < date2) {
+          result.push(`Wait for it: ${data[x].title}`);
+        }
+      } else if (month > month2) {
+        result.push(`To late: ${data[x].title}`);
+      } else if (month < month2) {
+        result.push(`Wait for it:${data[x].title}`);
+      }
+    } else if (year > year2) {
+      result.push(`To late: ${data[x].title}`);
+    } else if (year < year2) {
+      result.push(`Wait for it: ${data[x].title}`);
+    }
+  }
+
+  return result;
 }
 
-reminder(tasks, currentDate);
+console.log(reminder(tasks, currentDate));
 
-// module.exports = reminder;
+export default reminder;
--- week03/add-matrices/src/index.js
+++ week03/add-matrices/src/index.js
@@ -1,37 +1,36 @@
+let start = new Date(); // start measuring time
 let matrix1 = [
-  [1, 2],
-  [1, 2],
-
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
 ];
 let matrix2 = [
-  [3, 2],
-  [5, 2],
-
+  [3, 2, 3, 4],
+  [5, 2, 3, 4],
+  [7, 2, 3, 4],
+  [9, 2, 3, 4],
 ];
-
 function addMatrices(matrix1, matrix2, o) {
   let arr = [];
   let sum;
-
   for (let x = 0; x < matrix1.length; x++) {
     for (let i = 0; i < matrix2[x].length; i++) {
       sum = matrix1[x][i] + matrix2[x][i];
       arr.push(sum);
     }
   }
-
   let bigArr = [];
   for (let i = 0; i < matrix2.length; i++) {
     let n = 0;
     bigArr.push(arr.splice(n, n + o));
     n += o;
   }
-
   return bigArr;
 }
-
-// addMatrices(matrix1, matrix2,4);
-
-console.log(addMatrices(matrix1, matrix2,2));
+// addMatrices(matrix1, matrix2, 4);
+console.log(addMatrices(matrix1, matrix2,4));
+let end = new Date(); // end measuring time
+alert(`The loop took ${end - start} ms`);
 
 // module.exports = addMatrices;
