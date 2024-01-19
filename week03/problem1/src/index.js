5878d0f0fab429ea2d1312a516a8077d44b76cf8
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-19 20:57:20 +0330

    complete question translate language

--- week03/problem1/src/index.js
+++ week03/problem1/src/index.js
@@ -1,7 +1,11 @@
 const englishCode = "en-US";
 const spanishCode = "es-ES";
 const iranianCode = "fa-IR";
-function getAboutUsLink(language){
-    //your code here...
+// Function for change Language
+function getAboutUsLink(language) {
+  if (language === englishCode) return "/about-us";
+  else if (language === spanishCode) return "/acerca-de";
+  else if (language === iranianCode) return "/درباره-ما";
 }
+
 module.exports = getAboutUsLink;
