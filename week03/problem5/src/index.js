924ef16baa8d1cd18c8cf6fd38fa82d9d1869893
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-23 01:21:00 +0330

    start problem5

--- week03/problem5/src/index.js
+++ week03/problem5/src/index.js
@@ -1,5 +1,17 @@
+// let num = [24];
+let primes = [2, 3, 5, 7, 11];
+let result = [];
+
 function findPrimes(num) {
-  // your code here...
+  for (i = 0; i < primes.length; i++) {
+    if (num % primes[i] === 0) {
+      result.push( primes[i]);
+    }
+  }
+
+  return result;
 }
 
-module.exports = findPrimes;
+console.log(findPrimes(100));
+
+// module.exports = findPrimes;
