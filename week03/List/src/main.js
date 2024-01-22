d0106736e1d2fdfdde978aa3b916203e0b9bc069
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-22 18:58:43 +0330

    update list question

--- week03/List/src/main.js
+++ week03/List/src/main.js
@@ -1,20 +1,20 @@
 // you can use this data to test your function
-// import data from "./data.json" assert { type: "json" };
+import data from "./data.json" assert { type: "json" };
 
-const list = require("./data.json");
+// const list = require("./data.json");
 
 function filterList(data, model_numbers = []) {
-  const keys = Object.entries(data);
   const box = [];
 
-  for (let key of keys) {
-    for (let x of key[1]) {
+  for (let key in data) {
+    for (let x of data[key]) {
       for (let i = 0; i < model_numbers.length; i++) {
         if (model_numbers[i] === x.model_number) {
           box.push(x);
         }
       }
     }
+    // console.log(data[key]);
   }
 
   if (box.length === 0) return "not found!";
@@ -22,7 +22,7 @@ function filterList(data, model_numbers = []) {
   return box;
 }
 
-// console.log(filterList(data, ["A6F512", "R754F"]));
+console.log(filterList(data, ["A6F512", "R754F"]));
 // console.log(filterList(data));
 
-module.exports = filterList;
+// module.exports = filterList;
