ec1b58354ea84b8a19616077aa2c9d3955c78e73
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-25 01:54:57 +0330

    solve caeser question

--- week03/CaesarCipher/src/index.js
+++ week03/CaesarCipher/src/index.js
@@ -1,5 +1,25 @@
 function caesarCipher(message, shift) {
-    // YOUE CODE HERE
+  let result = "";
+
+  for (let i = 0; i < message.length; i++) {
+    let char = message[i];
+
+    if (char.match(/[a-z]/i)) {
+      const code = message.charCodeAt(i);
+
+      if (code >= 65 && code <= 90) {
+        char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+      } else if (code >= 97 && code <= 122) {
+        char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+      }
+    }
+
+    result += char;
+  }
+
+  return result;
 }
+
+console.log(caesarCipher("hello , world!", 4));
 
 module.exports = caesarCipher;
