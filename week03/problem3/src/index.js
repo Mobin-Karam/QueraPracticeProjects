4ee3c0cf15f5d20d13b4708698b5c6ee10f15d77
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-24 21:22:04 +0330

    complete 2 question of week03

--- week03/problem3/src/index.js
+++ week03/problem3/src/index.js
@@ -1,5 +1,30 @@
 function filterByTerm(inputArr, searchTerm) {
-    //your code here...
+  // Save Position of SearchTerm
+  let pos;
+  // Save Result and Return it
+  let result = [];
+  // loop around inputArr and find searchTerm
+  for (let i = 0; i < inputArr.length; i++) {
+    if (inputArr[i].url.indexOf(searchTerm)) {
+      pos = inputArr[i].url.indexOf(searchTerm.toLowerCase());
+      if (pos > 0) {
+        result.push(inputArr[i]);
+      }
+    }
   }
+
+  return result;
+}
+
+// console.log(
+//   filterByTerm(
+//     [
+//       { id: 1, url: "https://www.url1.dev" },
+//       { id: 2, url: "https://www.link2.dev" },
+//       { id: 3, url: "https://www.link3.dev" },
+//     ],
+//     "LINK"
+//   )
+// );
 
 module.exports = filterByTerm;
