0ad13dd3d993ad670869e0b4c346c733d71c1e89
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-26 22:39:58 +0330

    complete CaeserMail

--- week03/CaesarMail/src/index.js
+++ week03/CaesarMail/src/index.js
@@ -1,7 +1,36 @@
-function toRoamn(number) {
-  // YOUR CODE HERE
+
+// Change Numbers to Roman Numbers
+function toRoman(num) {
+  // List of Roman Numbers
+  let romanToNum = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let roman = "";
+  for (let key in romanToNum) {
+    while (num >= romanToNum[key]) {
+      // add Roman num
+      roman += key;
+      // increase Number that we give to function for measure it again
+      num -= romanToNum[key];
+    }
+  }
+  return roman;
 }
 
+// console.log(toRoman(1495));
 
+// console.log(1495);
 
-// module.exports = toRoamn;
+module.exports = toRoamn;
--- week03/Moderate/src/index.js
+++ week03/Moderate/src/index.js
@@ -1,33 +1,67 @@
 // define a function that takes three sets of numbers as parameters
+// function moderate(arr1, arr2, arr3) {
+//   let totalArray = arr1.concat(arr2, arr3);
+//   let average = 0;
+//   let median = 0;
+//   let mode = 0;
+//   let middle = 0;
+//   // Median
+//   if (totalArray.length % 2 === 0) {
+//     totalArray.sort((a, b) => a - b);
+//     middle = Math.floor(totalArray.length / 2);
+//     median = (totalArray[middle] + totalArray[middle - 1]) / 2;
+//   } else {
+//     totalArray.sort((a, b) => a - b);
+//     median = Math.floor(totalArray.length / 2);
+//   }
+//   // Average
+//   for (let num of totalArray) {
+//     average += num;
+//   }
+//   average = Number((average / totalArray.length).toFixed(2));
+//   // Mode
+//   totalArray.sort((a, b) => b - a);
+//   let frequency = {};
+//   let maxFrequency = 0;
+
+//   for (const number of totalArray) {
+//     frequency[number] = (frequency[number] || 0) + 1;
+//     if (frequency[number] > maxFrequency) {
+//       maxFrequency = frequency[number];
+//       mode = number;
+//     }
+//   }
+
+//   return {
+//     average: average,
+//     median: median,
+//     mode: mode,
+//   };
+// }
+
+// console.log(moderate([6], [4], []));
 function moderate(arr1, arr2, arr3) {
-  let average = 0;
-  let median = 0;
-  let mode = 0;
-  let middle = 0;
+  // Filter out non-numeric elements//
+  let numbers = arr1.concat(arr2, arr3);
 
-  // Average
-  let totalArray = arr1.concat(arr2, arr3);
-  for (let num of totalArray) {
-    average += num;
-  }
-  average = Number((average / totalArray.length).toFixed(2));
+  // Calculate the average
+  const sum = numbers.reduce((a, b) => a + b, 0);
+  const avg = sum / numbers.length;
 
-  // Median
-  if (totalArray.length % 2 === 0) {
-    totalArray.sort((a, b) => a - b);
-    middle = Math.floor(totalArray.length / 2);
-    median = (totalArray[middle] + totalArray[middle - 1]) / 2;
-  } else {
-    totalArray.sort((a, b) => a - b);
-    median = Math.floor(totalArray.length / 2);
-  }
+  // Calculate the median
+  let sortNumbers = numbers.slice().sort((a, b) => a - b);
+  const middle = Math.floor(sortNumbers.length / 2);
+  const median =
+    sortNumbers.length % 2 === 0
+      ? (sortNumbers[middle - 1] + sortNumbers[middle]) / 2
+      : sortNumbers[middle];
 
-  // Mode
-  totalArray.sort((a, b) => b - a);
-  let frequency = {};
+  // Calculate the mode
+  const frequency = {};
   let maxFrequency = 0;
+  let mode = null;
 
-  for (const number of totalArray) {
+  for (const number of numbers) {
     frequency[number] = (frequency[number] || 0) + 1;
     if (frequency[number] > maxFrequency) {
       maxFrequency = frequency[number];
@@ -36,12 +70,11 @@ function moderate(arr1, arr2, arr3) {
   }
 
   return {
-    average: average,
+    average: avg,
     median: median,
     mode: mode,
   };
 }
-
 console.log(moderate([6], [4], []));
 
 // module.exports = moderate;
