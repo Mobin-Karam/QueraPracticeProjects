479f7886b49d507df1ea79c356e7a907f9f305e0
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-29 13:13:57 +0330

    complete server question

--- week04/Server/src/main.js
+++ week04/Server/src/main.js
@@ -1,8 +1,27 @@
 // use request function to send a fake request to a server
-const request = require('./request');
+const request = require("./request.js");
 
-function checkPerformance(requestCount) {
-       // YOUR CODE HERE
+async function checkPerformance(requestCount) {
+  let successTimes = [];
+  let failureTimes = [];
+  for (let i = 0; i < requestCount; i++) {
+    try {
+      const time = await request();
+      successTimes.push(time);
+    } catch (time) {
+      failureTimes.push(time);
+    }
+  }
+  let successAverage =
+    successTimes.reduce((a, b) => a + b, 0) / successTimes.length || 0;
+  let failureAverage =
+    failureTimes.reduce((a, b) => a + b, 0) / failureTimes.length || 0;
+  let performance = (successTimes.length / requestCount) * 100;
+  return { successAverage, failureAverage, performance };
 }
 
-module.exports = checkPerformance
+// checkPerformance(11)
+//   .then((result) => console.log(result))
+//   .catch((error) => console.error(error));
+
+module.exports = checkPerformance;
