4630c7dca433887ff938a50df4d878a9351b1657
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-03 06:57:45 +0330

    start week05 , question Contacts

---
+++ week05/contacts/src/components/Contact/Contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Contact = ({
+  id,
+  firstName,
+  lastName,
+  email,
+  city,
+  checkboxHandler,
+  checked,
+}) => {
+  return (
+    <div className="contacts-list__item">
+      <div>
+        <h2>First Name: {firstName}</h2>
+        <h2>Last Name: {lastName}</h2>
+      </div>
+
+      <div>
+        <span className="email">{email}</span>
+      </div>
+
+      <div>
+        <p>City: {city}</p>
+        <input
+          type="checkbox"
+          value={id}
+          onChange={checkboxHandler}
+          checked={checked}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
