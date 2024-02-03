674f781e3e227904aa6bf062c2dce16e7e953c17
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-03 15:28:04 +0330

    complete Contact Question

--- week05/contacts/src/App.js
+++ week05/contacts/src/App.js
@@ -42,21 +42,22 @@ const initialValues = [
 const App = () => {
   const [people, setPeople] = useState(initialValues);
 
+  // Check Box Handler
   let checked;
-  let values = [];
-
   function checkboxHandler(event) {
-    if (!checked) {
-      checked = event.target.checked;
-      values.push({ id: event.target.value });
-      console.log(values);
-      console.log(checked);
-    } else {
-      checked = false;
-      values.filter((val) => deمثفث val.id);
-      console.log(values);
-      console.log(checked);
-    }
+    setPeople(
+      people.map((person) =>
+        person.id === parseInt(event.target.value, 10)
+          ? { ...person, checked: event.target.checked }
+          : person
+      )
+    );
+    console.log(people);
+    console.log("click");
+  }
+
+  function handleDeletePerson() {
+    setPeople(people.filter((person) => !person.checked));
   }
 
   return (
@@ -75,7 +76,7 @@ const App = () => {
                 email={person.email}
                 city={person.city}
                 checked={checked}
-                checkboxHandler={checkboxHandler}
+                onChangeHandle={checkboxHandler}
               />
             );
           })}
@@ -87,117 +88,12 @@ const App = () => {
       <section className="cta-section">
         <p className="text-2xl">Selected:</p>
         <p>
-          {/* در این تگ باید شماره ی هر آیتم که کلید می شود رندر شود، به طور مثال: 1,2 */}
-        </p>
-        <button
-          className="cta"
-          // should trigger an event when clicked
-        >
-          Delete Selected Items
-        </button>
-      </section>
-    </div>
-  );
-};
-
-export default App;
-
-// ===============
-import React, { useState } from "react";
-import Contact from "./components/Contact/Contact";
-
-const initialValues = [
-  {
-    id: 1,
-    first_name: "Kennie",
-    last_name: "Garth",
-    email: "kgarth0@ovh.net",
-    city: "Megati Kelod",
-  },
-  {
-    id: 2,
-    first_name: "Free",
-    last_name: "Eltringham",
-    email: "feltringham1@weebly.com",
-    city: "Haradzishcha",
-  },
-  {
-    id: 3,
-    first_name: "Isadore",
-    last_name: "Valintine",
-    email: "ivalintine2@issuu.com",
-    city: "El Benque",
-  },
-  {
-    id: 4,
-    first_name: "Annora",
-    last_name: "Neilus",
-    email: "aneilus3@uol.com.br",
-    city: "Uchaly",
-  },
-  {
-    id: 5,
-    first_name: "Tabbie",
-    last_name: "Rizzini",
-    email: "trizzini4@cpanel.net",
-    city: "Pora",
-  },
-];
-
-const App = () => {
-  const [people, setPeople] = useState(initialValues);
-
-  let checked;
-  let values = [];
-
-  function checkboxHandler(event) {
-    if (!checked) {
-      checked = event.target.checked;
-      values.push({ id: event.target.value });
-      console.log(values);
-      console.log(checked);
-    } else {
-      checked = false;
-      values.filter((val) => deمثفث val.id);
-      console.log(values);
-      console.log(checked);
-    }
-  }
-
-  return (
-    <div data-testid="app" className="app">
-      {/* ----------------------------------left side------------------------------------- */}
-
-      <section className="contacts-section">
-        <div className="contacts-list">
-          {people.map((person) => {
-            return (
-              <Contact
-                key={person.id}
-                id={person.id}
-                firstName={person.first_name}
-                lastName={person.last_name}
-                email={person.email}
-                city={person.city}
-                checked={checked}
-                checkboxHandler={checkboxHandler}
-              />
-            );
+          {people.map((p) => {
+            if (p.checked) return p.id;
+            else return "";
           })}
-        </div>
-      </section>
-
-      {/* ----------------------------------right side------------------------------------- */}
-
-      <section className="cta-section">
-        <p className="text-2xl">Selected:</p>
-        <p>
-          {/* در این تگ باید شماره ی هر آیتم که کلید می شود رندر شود، به طور مثال: 1,2 */}
         </p>
-        <button
-          className="cta"
-          // should trigger an event when clicked
-        >
+        <button className="cta" onClick={handleDeletePerson}>
           Delete Selected Items
         </button>
       </section>
--- week05/contacts/src/components/Contact/Contact.js
+++ week05/contacts/src/components/Contact/Contact.js
@@ -1,61 +1,32 @@
 import React from "react";
 
 const Contact = ({
-  id,
   firstName,
   lastName,
-  email,
+  id,
   city,
-  checkboxHandler,
+  email,
   checked,
+  onChangeHandle,
 }) => {
-  
   return (
     <div className="contacts-list__item">
       <div>
-        <h2>First Name: {firstName}</h2>
-        <h2>Last Name: {lastName}</h2>
+        <h2>First Name: {firstName} </h2>
+        <h2>Last Name: {lastName} </h2>
       </div>
 
       <div>
-        <span className="email">{email}</span>
+        <span className="email"> {email} </span>
       </div>
 
       <div>
-        <p>City: {city}</p>
+        <p>City: {city} </p>
         <input
           type="checkbox"
           value={id}
           checked={checked}
-          onChange={checkboxHandler}
-        />
-      </div>
-    </div>
-  );
-};
-
-export default Contact;
-
-/// =================
-import React from "react";
-
-const Contact = () => {
-  return (
-    <div className="contacts-list__item">
-      <div>
-        <h2>First Name: {/* first name */}</h2>
-        <h2>Last Name: {/* last name */}</h2>
-      </div>
-
-      <div>
-        <span className="email">{/* email */}</span>
-      </div>
-
-      <div>
-        <p>City: {/* city */}</p>
-        <input
-          type="checkbox"
-          // add needed props
+          onChange={onChangeHandle}
         />
       </div>
     </div>
